e0ee0a29abc73daee47521a1fd05620b89b0b6b5
Md Shamir Raza Quraishi <143636691+Md-Shamir-raza@users.noreply.github.com>
2024-02-17 19:54:45 +0530

    Solved assignment files are added

---
+++ firstassignment.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    cout<<"Enter the number to be displayed:"<<endl;
+    cin>>i>>j;
+    cout<<"The number you entered is  :"<<i<<" and "<<j<<endl;
+
+    return 0;
+}
